cc0c43f9a170aee24849a375d8549a90d2044e59
luccr1 <lucca.hellriegel@gmx.net>
2019-11-13 20:57:50 +0100

    removed global vars from app.js

--- js/app.js
+++ js/app.js
@@ -17,9 +17,6 @@
 
     //TODO: var to let and const
     //TODO: eslint
-    var player;
-    var enemies;
-    var cursors;
     var game = new Phaser.Game(config);
 
     function preload ()
@@ -32,24 +29,25 @@
     function create ()
     {
         let playerGroup = this.physics.add.group();
-        player = new CircleHBWithWeapon(this,"character",playerGroup, 100,450)
-        //this.physics.world.enable(player)
+        let playerWeaponGroup = this.physics.add.group()
+        game.player = new CircleHBWithWeapon(this,"character",playerGroup, 100,450, playerWeaponGroup)
    
         createAnims(this.anims)
         
-        this.cameras.main.startFollow(player);
-        enemies = this.physics.add.group();
+        this.cameras.main.startFollow(game.player);
+        const enemies = this.physics.add.group();
+        const enemyWeapons = this.physics.add.group();
         for (let index = 0; index < 2; index++) {
-            new CircleHBWithWeapon(this,"character",enemies,(index*70)+12,200)
+            new Enemy(this,"character",enemies,(index*70)+12,200, enemyWeapons)
         }
        
-        this.physics.add.overlap(player.weapon, enemies, enemyCollision, null, this);
-        cursors = this.input.keyboard.createCursorKeys();
-
+        this.physics.add.overlap(playerWeaponGroup, enemies, enemyCollision, null, this);
+        this.physics.add.overlap(enemyWeapons, playerGroup, enemyCollision, null, this);
+        game.cursors = this.input.keyboard.createCursorKeys();
         setupMovement(this.input, this.cameras, this.time)
     }
 
     function update ()
     {
-        checkMovement(this)
+        checkMovement()
     }
