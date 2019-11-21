849f99b3f023c0bf0d171d5d0134a748a513a78e
luccr1 <lucca.hellriegel@gmx.net>
2019-11-21 12:16:21 +0100

    more meaningful name

--- js/enemy/EnemyCircle.js
+++ js/enemy/EnemyCircle.js
@@ -1,20 +1,27 @@
-import { CircleWithRandWeapon } from "../unit/CircleWithRandWeapon";
+import {
+    CircleWithRandWeapon
+} from "../unit/CircleWithRandWeapon";
 export class EnemyCircle extends CircleWithRandWeapon {
     constructor(scene, x, y, texture, physicsGroup, weaponGroup) {
         super(scene, x, y, texture, physicsGroup, weaponGroup);
         this.hasBeenAttacked = false;
     }
-    preUpdate(time, delta) {
-        super.preUpdate(time, delta);
+
+    moveAndTurnToPlayer() {
         if (this.hasBeenAttacked) {
             this.scene.physics.moveToObject(this, this.scene.player, 100);
             let angle = Phaser.Math.Angle.Between(this.x, this.y, this.scene.player.x +
                 this.scene.cameras.main.scrollX, this.scene.player.y +
-            this.scene.cameras.main.scrollY);
+                this.scene.cameras.main.scrollY);
             this.setRotation(angle);
-            if (Phaser.Math.Distance.Between(this.x, this.y, this.scene.player.x, this.scene.player.y) < 100) {
-                this.attack();
-            }
+        }
+    }
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+        this.moveAndTurnToPlayer()
+        if (Phaser.Math.Distance.Between(this.x, this.y, this.scene.player.x, this.scene.player.y) < 100) {
+            this.attack();
+
         }
     }
     damage(amount) {
