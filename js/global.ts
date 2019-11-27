969acd432900716b3f80df99761979a906d0552d
luccr1 <lucca.hellriegel@gmx.net>
2019-11-27 21:24:50 +0100

    removed global world size vars

--- js/global.ts
+++ js/global.ts
@@ -1,9 +1,5 @@
 export const debugModus = false
 
-//TODO: dont fit areas into world, fit world around areas
-export const worldWidth = 2000
-export const worldHeight = 2000
-
 export const wallPartRadius = 40
 export const normalCircleRadius = 30
 
