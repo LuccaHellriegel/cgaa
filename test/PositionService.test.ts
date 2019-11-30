64aaa0b94bd0dd5912a79956d542cc2f8d41c979
luccr1 <lucca.hellriegel@gmx.net>
2019-11-30 15:23:02 +0100

    tests for Building spawn distance

--- test/PositionService.test.ts
+++ test/PositionService.test.ts
@@ -86,35 +86,113 @@ describe("Test PositionService", function() {
       let wallArea = { x: x, y: y, numberOfXRects: 5, numberOfYRects: 3 };
       let { row, column } = PositionService.findCurRelativePosInWallArea(
         wallArea,
-        x-7,
-        y-7
+        x - 7,
+        y - 7
       );
       expect(row).to.equal(2);
       expect(column).to.equal(2);
     });
     it("X and Y should be snapped back to 200", function() {
-      let {newX,newY} = PositionService.snapXYToGrid(200-7,200-7)
-      
-      expect(newX).to.equal(200);
-      expect(newY).to.equal(200);
+      let { newX, newY } = PositionService.snapXYToGrid(
+        5 * wallPartRadius - 7,
+        5 * wallPartRadius - 7
+      );
+
+      expect(newX).to.equal(5 * wallPartRadius);
+      expect(newY).to.equal(5 * wallPartRadius);
     });
     it("X and Y should remain 200", function() {
-      let {newX,newY} = PositionService.snapXYToGrid(200,200)
-      
-      expect(newX).to.equal(200);
-      expect(newY).to.equal(200);
+      let { newX, newY } = PositionService.snapXYToGrid(
+        5 * wallPartRadius,
+        5 * wallPartRadius
+      );
+
+      expect(newX).to.equal(5 * wallPartRadius);
+      expect(newY).to.equal(5 * wallPartRadius);
     });
     it("If X and Y is between tiles it should go to the closest (the right one)", function() {
-      let {newX,newY} = PositionService.snapXYToGrid(82,40)
-      
-      expect(newX).to.equal(120);
-      expect(newY).to.equal(40);
+      let { newX, newY } = PositionService.snapXYToGrid(
+        2 * wallPartRadius + 2,
+        wallPartRadius
+      );
+
+      expect(newX).to.equal(3 * wallPartRadius);
+      expect(newY).to.equal(wallPartRadius);
     });
     it("If X and Y is between tiles it should go to the closest (the left one)", function() {
-      let {newX,newY} = PositionService.snapXYToGrid(78,40)
-      
-      expect(newX).to.equal(40);
-      expect(newY).to.equal(40);
+      let { newX, newY } = PositionService.snapXYToGrid(
+        2 * wallPartRadius - 2,
+        wallPartRadius
+      );
+
+      expect(newX).to.equal(wallPartRadius);
+      expect(newY).to.equal(wallPartRadius);
+    });
+  });
+  describe("Check if new building is on top of old ones or their spawn areas", function() {
+    it("New building is in the same place as old one, so yes ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = rectBuildingHalfWidth
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is in the same place as second one, so yes ", function() {
+      let buildings = [{x: 4* rectBuildingHalfWidth, y: 4* rectBuildinghalfHeight}, {x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = rectBuildingHalfWidth
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is overlapping on the right, so yes ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = 2* rectBuildingHalfWidth
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is overlapping spawn area on the right, so yes ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = 3* rectBuildingHalfWidth + wallPartRadius
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is overlapping on the bottom, so yes ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = rectBuildingHalfWidth
+      let randY = 2*rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is overlapping spawn area on the bottom, so yes ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = rectBuildingHalfWidth
+      let randY = 3*rectBuildinghalfHeight + wallPartRadius
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.true
+    });
+    it("New building is exactly far enough away to the right, so no ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = 3*rectBuildingHalfWidth + 2*wallPartRadius
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.false
+    });
+    it("New building is more than far enough away to the right, so no ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = 3*rectBuildingHalfWidth + 4*wallPartRadius
+      let randY = rectBuildinghalfHeight
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+      expect(isOnTop).to.be.false
+    });
+    it("New building is exactly far enough away to the bottom, so no ", function() {
+      let buildings = [{x: rectBuildingHalfWidth, y: rectBuildinghalfHeight}]
+      let randX = rectBuildingHalfWidth
+      let randY = 3*rectBuildinghalfHeight + 2*wallPartRadius
+      let isOnTop = PositionService.checkIfOnTopOfOtherBuildingOrSpawnArea(buildings, randX, randY)
+
+      expect(isOnTop).to.be.false
     });
   });
 });
