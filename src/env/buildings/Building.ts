ca85436251e597f7c4d829ae75a5e3deda48c69a
luccr1 <lucca.hellriegel@gmx.net>
2019-12-05 17:04:55 +0100

    valid pos calc from Building to Populator

--- src/env/buildings/Building.ts
+++ src/env/buildings/Building.ts
@@ -1,17 +1,11 @@
 import { Image } from "../../base/BasePhaser";
-import { SpawnService } from "../../spawn/SpawnService";
 import { Area } from "../areas/Area";
-import { PositionService } from "../../services/PositionService";
 
 export class Building extends Image {
-  validSpawnPositions: any[];
-  enemies: any[] = [];
   area: Area;
 
   constructor(scene, x, y, physicsGroup, area) {
     super(scene, x, y, "rectBuilding", physicsGroup);
     this.area = area;
-    let { column, row } = PositionService.realPosToRelativePosInEnv(this.x, this.y);
-    this.validSpawnPositions = SpawnService.calculateRelativeSpawnPositionsAround(column, row, 3, 1);
   }
 }
--- src/units/populators/BuildingPopulator.ts
+++ src/units/populators/BuildingPopulator.ts
@@ -4,10 +4,12 @@ import { Building } from "../../env/buildings/Building";
 import EasyStar from "easystarjs";
 import { SpawnService } from "../../spawn/SpawnService";
 import { EnemyCircle } from "../circles/EnemyCircle";
+import { PositionService } from "../../services/PositionService";
 
 export class BuildingPopulator extends Populator {
   easyStar: EasyStar.js;
   building: Building;
+  validSpawnPositions: any[];
 
   constructor(
     scene: Gameplay,
@@ -17,12 +19,15 @@ export class BuildingPopulator extends Populator {
   ) {
     super(scene, enemyPhysicsGroup, weaponPhysicsGroup);
     this.building = building;
+    let { column, row } = PositionService.realPosToRelativePosInEnv(building.x, building.y);
+    this.validSpawnPositions = SpawnService.calculateRelativeSpawnPositionsAround(column, row, 3, 1);
   }
 
+  //TODO: building has all the possible paths from spawn pos to exit already and here we assign it
   //TODO: use spawnmanager
-  calculateRandUnitSpawnPosition(building) {
+  calculateRandUnitSpawnPosition() {
     return SpawnService.randomlyTryAllSpawnablePosInRelationToEnv(
-      building.validSpawnPositions,
+      this.validSpawnPositions,
       spawnablePosCount => {
         return Phaser.Math.Between(0, spawnablePosCount);
       },
@@ -33,7 +38,7 @@ export class BuildingPopulator extends Populator {
   }
 
   createEnemy() {
-    let spawnPositon = this.calculateRandUnitSpawnPosition(this.building);
+    let spawnPositon = this.calculateRandUnitSpawnPosition();
     if (spawnPositon === null) return null;
 
     let { randX, randY } = spawnPositon;
