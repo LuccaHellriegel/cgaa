e2bcdcb22dc5e1a75bfebef426d7f2d22ad699df
luccr1 <lucca.hellriegel@gmx.net>
2019-12-16 23:06:02 +0100

    dont start waves of destroyed camps

--- src/game/enemies/wave.ts
+++ src/game/enemies/wave.ts
@@ -1,12 +1,22 @@
 import { Gameplay } from "../../scenes/Gameplay";
 import { campColors } from "../base/globals/globalColors";
+import { executeOverAllCamps } from "../base/globals/global";
 
 export function spawnWave(scene: Gameplay, index) {
+	let destroyedCamps = [];
+	executeOverAllCamps(color => {
+		scene.events.on("destroyed-" + color, () => {
+			destroyedCamps.push(color);
+		});
+	});
+
 	let prevIndex = index - 1;
 	if (index === 0) prevIndex = campColors.length;
 	scene.events.emit("end-wave-" + campColors[prevIndex]);
 
-	scene.events.emit("start-wave-" + campColors[index]);
+	let curColor = campColors[index];
+	if (!destroyedCamps.includes(curColor)) scene.events.emit("start-wave-" + curColor);
+
 	index++;
 	if (index === campColors.length) index = 0;
 	scene.time.addEvent({
