e107b763ad2f21066ffd3a462d23c686236f4918
luccr1 <lucca.hellriegel@gmx.net>
2020-01-20 14:17:30 +0100

    extracted Circle state handling to EnemyState

--- src/game/enemies/unit/EnemyCircle.ts
+++ src/game/enemies/unit/EnemyCircle.ts
@@ -2,10 +2,8 @@ import { HealthBar } from "../../base/ui/HealthBar";
 import { Circle } from "../../base/classes/Circle";
 import { PathContainer } from "../path/classes/PathContainer";
 import { damageable } from "../../base/interfaces";
-import { WallPart } from "../../area/wall/WallPart";
-import { Building } from "../camp/building/Building";
-import { gridPartHalfSize } from "../../base/globals/globalSizes";
 import { PoolHelper } from "../../base/pool/PoolHelper";
+import { EnemyState } from "./EnemyState";
 
 export class EnemyCircle extends Circle implements damageable {
 	healthbar: HealthBar;
@@ -15,11 +13,13 @@ export class EnemyCircle extends Circle implements damageable {
 	spotted: any;
 	pursuing: any;
 	barrier: any;
+	private stateHandler: EnemyState;
 
-	constructor(config, private velo: number) {
+	constructor(config, public velo: number) {
 		super(config);
 		this.healthbar = config.healthbar;
 		this.color = config.color;
+		this.stateHandler = new EnemyState(this);
 	}
 
 	damage(amount) {
@@ -47,113 +47,6 @@ export class EnemyCircle extends Circle implements damageable {
 	preUpdate(time, delta) {
 		super.preUpdate(time, delta);
 		this.healthbar.move(this.x, this.y);
-		this.executeState();
-	}
-
-	private executeState() {
-		let state = this.state;
-		switch (state) {
-			case "idle": {
-				this.idle();
-				break;
-			}
-			case "guard": {
-				this.guard();
-				break;
-			}
-			case "ambush": {
-				this.ambush();
-				break;
-			}
-			case "obstacle": {
-				this.obstacle();
-				break;
-			}
-		}
-	}
-
-	private obstacle() {
-		this.setVelocity(0, 0);
-		if (this.barrier) {
-			this.moveBack();
-			if (this.barrier instanceof WallPart || this.barrier instanceof Building) {
-				if (this.pathContainer) {
-					this.state = "ambush";
-				}
-			} else {
-				this.turnTo(this.barrier);
-				this.spotted = this.barrier;
-				this.state = "guard";
-			}
-		}
-	}
-
-	private moveBack() {
-		let x = this.x;
-		let y = this.y;
-		let angle = Phaser.Math.Angle.Between(this.barrier.x, this.barrier.y, x, y);
-
-		let bounceBackDistance = 1;
-		let x1 = x + Math.cos(angle) * bounceBackDistance;
-		let y1 = y + Math.sin(angle) * bounceBackDistance;
-		this.setPosition(x1, y1);
-	}
-
-	private idle() {
-		this.setVelocity(0, 0);
-		if (this.pathContainer) {
-			this.state = "ambush";
-		} else {
-			this.state = "guard";
-		}
-	}
-
-	private spottedOutOfSight() {
-		let dist = Phaser.Math.Distance.Between(this.x, this.y, this.spotted.x, this.spotted.y);
-		return dist > 6 * gridPartHalfSize;
-	}
-
-	private guard() {
-		if (!this.spotted || this.spottedOutOfSight() || !this.spotted.scene) {
-			this.spotted = undefined;
-			this.state = "idle";
-		} else {
-			this.turnTo(this.spotted);
-			let inReach = this.moveTo(this.spotted);
-			if (inReach) this.attack();
-		}
-	}
-
-	private turnTo(obj) {
-		let newRotation = Phaser.Math.Angle.Between(this.x, this.y, obj.x, obj.y);
-		let correctionForPhasersMinus90DegreeTopPostion = (Math.PI / 180) * 90;
-		this.setRotation(newRotation + correctionForPhasersMinus90DegreeTopPostion);
-	}
-
-	private moveTo(target) {
-		let reachDist = this.weapon.polygonArr[this.weapon.polygonArr.length - 1].height;
-		let inReach = Phaser.Math.Distance.Between(this.x, this.y, target.x, target.y) - 15 < reachDist;
-		if (!inReach) {
-			this.scene.physics.moveToObject(this, target, this.velo);
-		} else {
-			this.setVelocity(0, 0);
-		}
-		return inReach;
-	}
-
-	private ambush() {
-		if (this.pathContainer.path && this.pathContainer.path[this.curPosInPath]) {
-			let x = this.pathContainer.path[this.curPosInPath].x;
-			let y = this.pathContainer.path[this.curPosInPath].y;
-
-			this.turnTo({ x, y });
-			if (Math.abs(this.x - x) < 2 && Math.abs(this.y - y) < 2) {
-				this.curPosInPath++;
-			} else {
-				this.scene.physics.moveTo(this, x, y, this.velo);
-			}
-		} else if (this.pathContainer.path && this.curPosInPath >= this.pathContainer.path.length) {
-			this.setVelocity(0, 0);
-		}
+		this.stateHandler.execute();
 	}
 }
---
+++ src/game/enemies/unit/EnemyState.ts
@@ -0,0 +1,114 @@
+import { EnemyCircle } from "./EnemyCircle";
+import { WallPart } from "../../area/wall/WallPart";
+import { Building } from "../camp/building/Building";
+import { gridPartHalfSize } from "../../base/globals/globalSizes";
+
+export class EnemyState {
+	constructor(private circle: EnemyCircle) {}
+
+	execute() {
+		switch (this.circle.state) {
+			case "idle": {
+				this.idle();
+				break;
+			}
+			case "guard": {
+				this.guard();
+				break;
+			}
+			case "ambush": {
+				this.ambush();
+				break;
+			}
+			case "obstacle": {
+				this.obstacle();
+				break;
+			}
+		}
+	}
+
+	private obstacle() {
+		this.circle.setVelocity(0, 0);
+		if (this.circle.barrier) {
+			this.moveBack();
+			if (this.circle.barrier instanceof WallPart || this.circle.barrier instanceof Building) {
+				if (this.circle.pathContainer) {
+					this.circle.state = "ambush";
+				}
+			} else {
+				this.turnTo(this.circle.barrier);
+				this.circle.spotted = this.circle.barrier;
+				this.circle.state = "guard";
+			}
+		}
+	}
+
+	private moveBack() {
+		let x = this.circle.x;
+		let y = this.circle.y;
+		let angle = Phaser.Math.Angle.Between(this.circle.barrier.x, this.circle.barrier.y, x, y);
+
+		let bounceBackDistance = 1;
+		let x1 = x + Math.cos(angle) * bounceBackDistance;
+		let y1 = y + Math.sin(angle) * bounceBackDistance;
+		this.circle.setPosition(x1, y1);
+	}
+
+	private idle() {
+		this.circle.setVelocity(0, 0);
+		if (this.circle.pathContainer) {
+			this.circle.state = "ambush";
+		} else {
+			this.circle.state = "guard";
+		}
+	}
+
+	private spottedOutOfSight() {
+		let dist = Phaser.Math.Distance.Between(this.circle.x, this.circle.y, this.circle.spotted.x, this.circle.spotted.y);
+		return dist > 6 * gridPartHalfSize;
+	}
+
+	private guard() {
+		if (!this.circle.spotted || this.spottedOutOfSight() || !this.circle.spotted.scene) {
+			this.circle.spotted = undefined;
+			this.circle.state = "idle";
+		} else {
+			this.turnTo(this.circle.spotted);
+			let inReach = this.moveTo(this.circle.spotted);
+			if (inReach) this.circle.attack();
+		}
+	}
+
+	private turnTo(obj) {
+		let newRotation = Phaser.Math.Angle.Between(this.circle.x, this.circle.y, obj.x, obj.y);
+		let correctionForPhasersMinus90DegreeTopPostion = (Math.PI / 180) * 90;
+		this.circle.setRotation(newRotation + correctionForPhasersMinus90DegreeTopPostion);
+	}
+
+	private moveTo(target) {
+		let reachDist = this.circle.weapon.polygonArr[this.circle.weapon.polygonArr.length - 1].height;
+		let inReach = Phaser.Math.Distance.Between(this.circle.x, this.circle.y, target.x, target.y) - 15 < reachDist;
+		if (!inReach) {
+			this.circle.scene.physics.moveToObject(this.circle, target, this.circle.velo);
+		} else {
+			this.circle.setVelocity(0, 0);
+		}
+		return inReach;
+	}
+
+	private ambush() {
+		if (this.circle.pathContainer.path && this.circle.pathContainer.path[this.circle.curPosInPath]) {
+			let x = this.circle.pathContainer.path[this.circle.curPosInPath].x;
+			let y = this.circle.pathContainer.path[this.circle.curPosInPath].y;
+
+			this.turnTo({ x, y });
+			if (Math.abs(this.circle.x - x) < 2 && Math.abs(this.circle.y - y) < 2) {
+				this.circle.curPosInPath++;
+			} else {
+				this.circle.scene.physics.moveTo(this.circle, x, y, this.circle.velo);
+			}
+		} else if (this.circle.pathContainer.path && this.circle.curPosInPath >= this.circle.pathContainer.path.length) {
+			this.circle.setVelocity(0, 0);
+		}
+	}
+}
